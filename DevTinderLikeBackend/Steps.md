b4d6a4fd268485763b79ef001e7db030be317a4d
Sadhananagori18 <44465515+Sadhananagori18@users.noreply.github.com>
2026-03-02 13:00:58 +0530

    exploring api calls

---
+++ DevTinderLikeBackend/Steps.md
@@ -0,0 +1,19 @@
+A)
+Create a repository
+Initialize the repository
+install express
+Create a server
+Listen on port
+Write Request Handler for call (ex - /test, /hello etc)
+Install nodemon and update scripts inside package.json
+
+B)
+initialize git
+create .gitignore
+Create a remote repo on github
+push all code on remote origin
+
+C)
+install postman app
+create workspace and collection
+test the call
--- DevTinderLikeBackend/src/app.js
+++ DevTinderLikeBackend/src/app.js
@@ -1,14 +1,35 @@
 const express = require("express")
 const app = express()
 
-app.use("/hello", (req, res) => {
-  res.send("Hello abcd page using express server")
+//order is very important in route.
+
+// app.use("/hello", (req, res) => {
+//   //Anything will start with "/hello" this will be matched. ex - /hello/jshfd/sjf etc
+//   res.send("Hello abcd page using express server")
+// })
+// app.use("/test", (req, res) => {
+//   //Anything will start with "/test" this will be matched. ex - /test/jshfd/sjf etc
+//   res.send("Hello test page using express server")
+// })
+// app.use("/", (req, res) => {
+//   // if any route that matches this and start with this "/", route will execute this callback. Anything will start with "/" this will be matched.
+//   res.send("Hello express server")
+// })
+
+//will not use app.use()
+
+app.get("/test", (req, res) => {
+  // get Data from db
+  res.send({ firstname: "honey", lastname: "bunny" })
 })
-app.use("/test", (req, res) => {
-  res.send("Hello test page using express server")
+
+app.post("/user", (req, res) => {
+  // Saving Data in db
+  res.send("Saving Data in database successfully")
 })
-app.use("/", (req, res) => {
-  res.send("Hello express server")
+
+app.delete("/deleteuser", (req, res) => {
+  res.send("Deleted user successfully")
 })
 
 app.listen(4000, () => {
